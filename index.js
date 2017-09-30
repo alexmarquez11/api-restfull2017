aa5bd4d131f18baf190336b5a1ebdf123961665d
alexmarquez11 <31642506+alexmarquez11@users.noreply.github.com>
2017-09-30 14:53:25 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const app = require('./app.js')
+const config = require('./config.js')
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.db, (err, res) => {
+  if (err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log('Conexion a la base de datos establecida')
+  app.listen(config.port, () => {
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+  })
+})
